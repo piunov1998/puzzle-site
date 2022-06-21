cc797d032e2a4ee0943b2fb34a24653156b252bc
Tony <piunov.doc@gmail.com>
2022-06-21 12:56:50 +0300

    Update api_test.py

--- routers/api_test.py
+++ routers/api_test.py
@@ -3,12 +3,12 @@ import flask
 test_router = flask.Blueprint('test', __name__, url_prefix='/test')
 
 
-@test_router.get('/')
+@test_router.post('/')
 def main():
     try:
-        print(flask.data)
+        print(flask.request.data.decode('utf-8'))
         with open('./log.txt', 'a', encoding='utf-8') as file:
-            file.write(flask.data)
+            file.write(flask.request.data.decode('utf-8'))
     except:
         pass
-    return flask.json, 200
+    return flask.request.json, 200
